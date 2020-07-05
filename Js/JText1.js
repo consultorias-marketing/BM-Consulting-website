dbb9ead540cb0572b5b78903e3b061f6e067274f
Asesoria-Consultoria <davidenriquemorales4@gmailcom>
2020-07-05 13:17:56 -0600

    footer Corregido

---
+++ Js/JText1.js
@@ -0,0 +1,4 @@
+(function(){
+    t1.textContent ="ACA-Consulting es una consultora nacional enfocada al desarrollo general de las empresas privadas y personas en general, por medio de mentoría y asesorías personalizadas en diversos temas de interés empresarial. Entre ellas: La Planificación estratégica, desarrollo de negocios, Marketing, sistemas de calidad y finanzas estratégicas.";
+
+}())
