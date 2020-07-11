1800db678ea0a4e88a4ca0cf12a90d91f69e7aa5
Asesoria-Consultoria <davidenriquemorales4@gmailcom>
2020-07-11 14:54:35 -0600

    Cositas #2 UwU

--- Js/JText2.js
+++ Js/JText2.js
@@ -1,8 +1,35 @@
 
-(function(){
-    t1.textContent =" Hola futuros empresarios y personas de éxito, me complace dirigirme a ustedes para extenderles una invitación a este blog en el cual obtendrán información valiosa sobre experiencias que he tenido en asesorías brindadas a empresas y sobre distintos temas de índole empresarial  y de emprendedurismo que sin lugar a duda te permitirán obtener mejores herramientas y pautas para lograr el desarrollo de tus capacidades y la realización de tu éxito.";
-    t2.textContent =" También compartiré los consejos y recomendaciones que empresarios de éxito del país brindan  a los microempresarios y público en general para que logren acercarse más a la deseada Libertad financiera. También estaré compartiendo plantillas, libros, manuales y tips sobre  ventas, atención al cliente, procesos de calidad, entre otros. Con el fin de brindarles herramientas confiables e importantes en la aplicación diaria en las empresas, negocios e incluso la familia. Sumado a esto tendremos videoconferencias en relación a temas económicos de vital importancia, de los cuales les dejaré un link y el programa de actividades a realizar y su horario.";
-    t3.textContent =" Espero lograr aportar al desarrollo de la productividad del capital humano en muchos rincones del mundo con los temas que abordaré en este blog. Nos vemos pronto.";
-    t4.textContent='Autor: Beral Morales Arce';
-    t5.textContent='Introducción/Presentación:';
-}())
+
+t1.textContent =" Hola futuros empresarios y personas de éxito, me complace dirigirme a ustedes para extenderles una invitación a este blog en el cual obtendrán información valiosa sobre experiencias que he tenido en asesorías brindadas a empresas y sobre distintos temas de índole empresarial  y de emprendedurismo que sin lugar a duda te permitirán obtener mejores herramientas y pautas para lograr el desarrollo de tus capacidades y la realización de tu éxito.";
+t2.textContent =" También compartiré los consejos y recomendaciones que empresarios de éxito del país brindan  a los microempresarios y público en general para que logren acercarse más a la deseada Libertad financiera. También estaré compartiendo plantillas, libros, manuales y tips sobre  ventas, atención al cliente, procesos de calidad, entre otros. Con el fin de brindarles herramientas confiables e importantes en la aplicación diaria en las empresas, negocios e incluso la familia. Sumado a esto tendremos videoconferencias en relación a temas económicos de vital importancia, de los cuales les dejaré un link y el programa de actividades a realizar y su horario.";
+t3.textContent =" Espero lograr aportar al desarrollo de la productividad del capital humano en muchos rincones del mundo con los temas que abordaré en este blog. Nos vemos pronto.";
+t4.textContent='Autor: Beral Morales Arce';
+t5.textContent='Introducción/Presentación:';
+
+
+l0.textContent ='Record Académico :';
+
+l1.textContent = ' Especialista en Gestión de Calidad ISO';
+l2.textContent = ' Senior en Planificación Estratégica empresarial';
+l3.textContent = ' Técnico Superior en Administración de Empresas';
+l4.textContent = ' Técnico Medio en Administración con Énfasis en Mercadeo y Ventas';
+l5.textContent = ' Auditor Interno de Sistemas de Gestión ISO 17025';
+
+l6.textContent ='Experiencia:';
+
+l7.textContent = ' Asesor y Consultor Externo de MIPYMES';
+l8.textContent = ' Distribuidora de pollo "Don Enoc"';
+l9.textContent = ' Comedor "EL Chele"';
+l10.textContent =' Centro Cultural "Batahola Norte"';
+l11.textContent = ' Multipocker Nicaragua';
+l12.textContent = ' Trainer en Atención y Servicio al cliente en PYMES';
+l13.textContent = ' Comedor "El Chele"';
+l14.textContent = ' Autos Alemanes S.A.';
+l15.textContent = ' Fundador de ACA-Consulting';
+l16.textContent = ' Consultora Planificación Estratégica';
+l17.textContent = ' Asesor y Consultor Externo de Grandes Empresas';
+l18.textContent = ' JDC Jarquín S.A.';
+l19.textContent = ' Compañía Cervecera de Nicaragua S.A.';
+l20.textContent = ' Refinería Puma Energy Bahamas S.A.';
+l21.textContent = ' Autos Alemanes y Asociados.';
+l22.textContent = ' Seminarista y conferencista independiente';
--- Js/pie.js
+++ Js/pie.js
@@ -1,6 +1,5 @@
-(function(){
-    p1.textContent =" Beral Morales";
-    p2.textContent =" +505 8151 0395";
-    p3.textContent =" +505 8151 0395";
-    p4.textContent =" Beralmorales55@gmail.com ";
-}())
+
+p1.textContent =" Beral Morales";
+p2.textContent =" +505 8151 0395";
+p3.textContent =" +505 8151 0395";
+p4.textContent =" Beralmorales55@gmail.com ";
